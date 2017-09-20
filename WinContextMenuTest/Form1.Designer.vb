265220385f48d5e826b9c7c8c2d03cf20cc22648
kazenetu <rakudafactory@gmail.com>
2017-09-21 06:30:07 +0900

    [WIP]サブメニューの動的生成

--- WinContextMenuTest/Form1.Designer.vb
+++ WinContextMenuTest/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,72 @@ Partial Class Form1
     'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
     'Windows フォーム デザイナーを使用して変更できます。  
     'コード エディターを使って変更しないでください。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.MenuItemDinamic = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuItemStatic = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 42)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(229, 224)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Test1" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "テスト2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(299, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "1.改行を入れてコンテキストメニューに表示するキーワードを入力"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuItemDinamic, Me.MenuItemStatic})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(132, 48)
+        '
+        'MenuItemDinamic
+        '
+        Me.MenuItemDinamic.Name = "MenuItemDinamic"
+        Me.MenuItemDinamic.Size = New System.Drawing.Size(152, 22)
+        Me.MenuItemDinamic.Text = "動的メニュー"
+        '
+        'MenuItemStatic
+        '
+        Me.MenuItemStatic.Name = "MenuItemStatic"
+        Me.MenuItemStatic.Size = New System.Drawing.Size(152, 22)
+        Me.MenuItemStatic.Text = "静的メニュー"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(462, 370)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents MenuItemDinamic As ToolStripMenuItem
+    Friend WithEvents MenuItemStatic As ToolStripMenuItem
 End Class
--- WinContextMenuTest/Form1.vb
+++ WinContextMenuTest/Form1.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Form1
+    Private Sub ContextMenuStrip1_Opening(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles ContextMenuStrip1.Opening
+        Me.MenuItemDinamic.DropDownItems.Clear()
 
+        For Each item In Me.TextBox1.Text.Split(Environment.NewLine )
+            Dim menuItem = Me.MenuItemDinamic.DropDownItems.Add(item)
+            'menuItem.AutoSize = True
+            'menuItem.AutoToolTip = False
+            'menuItem.Height = 22
+            'menuItem.Width = 120
+            menuItem.TextAlign = ContentAlignment.MiddleCenter
+            menuItem.TextDirection = ToolStripTextDirection.Horizontal
+        Next
+
+        ContextMenuStrip1.Update()
+
+        e.Cancel = False
+    End Sub
 End Class
