00dd792c371e6b7a99c92f1a0589bb885abdfbb4
kazenetu <rakudafactory@gmail.com>
2017-09-22 07:13:42 +0900

    サブメニューの高さが2倍に設定される不具合を修正

--- WinContextMenuTest/Form1.vb
+++ WinContextMenuTest/Form1.vb
@@ -2,14 +2,8 @@
     Private Sub ContextMenuStrip1_Opening(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles ContextMenuStrip1.Opening
         Me.MenuItemDinamic.DropDownItems.Clear()
 
-        For Each item In Me.TextBox1.Text.Split(Environment.NewLine )
-            Dim menuItem = Me.MenuItemDinamic.DropDownItems.Add(item)
-            'menuItem.AutoSize = True
-            'menuItem.AutoToolTip = False
-            'menuItem.Height = 22
-            'menuItem.Width = 120
-            menuItem.TextAlign = ContentAlignment.MiddleCenter
-            menuItem.TextDirection = ToolStripTextDirection.Horizontal
+        For Each item In Me.TextBox1.Text.Split(Environment.NewLine)
+            Me.MenuItemDinamic.DropDownItems.Add(item.Replace(vbLf, String.Empty))
         Next
 
         ContextMenuStrip1.Update()
